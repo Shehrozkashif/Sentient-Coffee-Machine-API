53c5bb13271b89f01003a97cc773c3927c7397d0
ShehrozKashif <sharooz57@gmail.com>
2024-11-24 17:06:44 +0500

    created a spring boot appication !

---
+++ src/main/java/com/api/restapi/RestapiApplication.java
@@ -0,0 +1,15 @@
+package com.api.restapi;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(RestapiApplication.class, args);
+
+		System.out.println("hello world");
+	}
+
+}

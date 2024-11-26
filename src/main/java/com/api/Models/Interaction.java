d1e94e87d858bf2f0c3c49f45dc19932a9fb63cd
ShehrozKashif <sharooz57@gmail.com>
2024-11-26 15:37:32 +0500

    added all models and controllers

---
+++ src/main/java/com/api/Models/Interaction.java
@@ -0,0 +1,5 @@
+package com.api.Models;
+
+public class Interaction {
+
+}
---
+++ src/main/java/com/api/controllers/InteractionController.java
@@ -0,0 +1,30 @@
+package com.api.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import com.api.Models.Interaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/interaction")
+public class InteractionController {
+    private final List<Interaction> interactions = new ArrayList<>();
+
+    @PostMapping
+    public String recordInteraction(@RequestBody Interaction interaction) {
+        interactions.add(interaction);
+        return "Interaction recorded!";
+    }
+
+    @GetMapping("/recommendations")
+    public List<String> getRecommendations(@RequestParam String userId) {
+     
+        List<String> recommendations = new ArrayList<>();
+        recommendations.add("Espresso");
+        recommendations.add("Latte");
+        return recommendations;
+    }
+}
